f2efede126348dcca0abaef155209b27c9e9b9cc
DandaMan99199 <76517272+DandaMan99199@users.noreply.github.com>
2021-01-11 14:45:27 +0100

    Delete StartServer.java

--- StartServer.java
+++
@@ -1,8 +0,0 @@
-public class StartServer{
-  public StartServer(){
-    
-  }                       
-  public static void main (String args[]){
-    QuizServer s=new QuizServer(50000);
-  }
-}                                                   
