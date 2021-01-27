f3187952b591cf8a0648b16e81752adc595afa7a
DandaMan99199 <76517272+DandaMan99199@users.noreply.github.com>
2021-01-27 12:16:00 +0100

    Add files via upload

---
+++ Prototyp 3.1/Spieler.java
@@ -0,0 +1,51 @@
+public class Spieler{
+  private int elo;
+  private String name;
+  private String ip;
+  private int port;
+  private int spiel;
+  private int nr;
+  private int punkte;
+  public Spieler(String pName, String pIp, int pPort, int pElo){
+    name=pName;
+    ip=pIp;
+    port=pPort;
+    elo=pElo;
+    spiel=0;
+    nr=0;
+    punkte=0;
+  }
+  public String getName(){
+    return name;
+  }
+  public String getIp(){
+    return ip;
+  }
+  public int getPort(){
+    return port;
+  }
+  public int getElo(){
+    return elo;
+  }
+  public void setSpiel(int pSpiel){
+    spiel=pSpiel;
+  }
+  public int getSpiel(){
+    return spiel;
+  }
+  public void setNr(int pNr){
+    nr=pNr;
+  }
+  public int getNr(){
+    return nr;
+  }
+  public void setPunkte(int pPunkte){
+    punkte=pPunkte;
+  }
+  public int getPunkte(){
+    return punkte;
+  }
+  public void setElo(int pElo){
+    elo=pElo;
+  }
+}
---
+++ Prototyp 3.1/StartServer.java
@@ -0,0 +1,8 @@
+public class StartServer{
+  public StartServer(){
+    
+  }                        
+  public static void main (String args[]){
+    QuizServer s=new QuizServer(50000);
+  }
+}                                                                                                
